22d6f1cc6fba7971bcf10305b56bc2b2a08cba73
arup-paul <aruppaulctg@gmail.com>
2023-04-17 21:48:21 +0600

    inheritance

--- app/DebCollector.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-interface DebCollector
-{
-    public function collect(float $amount): float;
-}
--- public/index.php
+++ public/index.php
@@ -1,13 +1,23 @@
 <?php
 
+use App\Toaster;
+
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 require __DIR__ .'/../vendor/autoload.php';
 
- $invoice = new App\Invoice(15);
-App\Invoice::process([1,2,3]);
+$toaster = new \App\ToasterPro();
+
+$toaster->addSlice('Bread');
+$toaster->addSlice('Bread');
+$toaster->addSlice('Bread');
+$toaster->addSlice('Bread');
+$toaster->addSlice('Bread');
+$toaster->toastBagel();
+
+
 
 
 
